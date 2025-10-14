9bf8e70ea9052cbbbf48089b40e9ae490c352281
유재욱 <jinsa6mm@naver.com>
2025-10-14 16:23:33 +0900

    home컨트롤러 .do 삭제

--- mondayLegacy/src/main/java/com/ezen/vo/HomeController.java
+++ mondayLegacy/src/main/java/com/ezen/vo/HomeController.java
@@ -14,27 +14,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController 
 {
 	// main ----------------------------------------------------------
-	@RequestMapping(value = "/about.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
 	public String About() 
 	{
 		return "about";
 	}
-	@RequestMapping(value = "/index.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String Index() 
 	{
 		return "index";
 	}
-	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String Login() 
 	{
 		return "insert";
 	}
-	@RequestMapping(value = "/signup.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String Signup() 
 	{
 		return "signup";
 	}
-	@RequestMapping(value = "/thankyou.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/thankyou", method = RequestMethod.GET)
 	public String Thankyou() 
 	{
 		return "thankyou";
@@ -42,37 +42,37 @@ public class HomeController
 	// main ----------------------------------------------------------
 	
 	// admin ----------------------------------------------------------
-	@RequestMapping(value = "/insert.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
 	public String Insert() 
 	{
 		return "insert";
 	}
-	@RequestMapping(value = "/list.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public String List() 
 	{
 		return "list";
 	}
-	@RequestMapping(value = "/adminlogin.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/adminlogin", method = RequestMethod.GET)
 	public String AdminLogin() 
 	{
 		return "admin/login";
 	}
-	@RequestMapping(value = "/order_list.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/order_list", method = RequestMethod.GET)
 	public String OrderList() 
 	{
 		return "order_list";
 	}
-	@RequestMapping(value = "/order_set.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/order_set", method = RequestMethod.GET)
 	public String OrderSet() 
 	{
 		return "order_set";
 	}
-	@RequestMapping(value = "/sales.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/sales", method = RequestMethod.GET)
 	public String Sales() 
 	{
 		return "sales";
 	}
-	@RequestMapping(value = "/update.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String Update() 
 	{
 		return "update";
@@ -80,17 +80,17 @@ public class HomeController
 	// admin ----------------------------------------------------------
 	
 	// help ----------------------------------------------------------
-	@RequestMapping(value = "/board_detail.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/board_detail", method = RequestMethod.GET)
 	public String BoardDetail() 
 	{
 		return "help/board_detail";
 	}
-	@RequestMapping(value = "/board.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/board", method = RequestMethod.GET)
 	public String Board() 
 	{
 		return "help/board";
 	}
-	@RequestMapping(value = "/inquire.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/inquire", method = RequestMethod.GET)
 	public String Inquire() 
 	{
 		return "help/inquire";
@@ -98,22 +98,22 @@ public class HomeController
 	// help ----------------------------------------------------------
 
 	// member ----------------------------------------------------------
-	@RequestMapping(value = "/cart.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/cart", method = RequestMethod.GET)
 	public String Cart() 
 	{
 		return "member/cart";
 	}
-	@RequestMapping(value = "/mypage_update.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/mypage_update", method = RequestMethod.GET)
 	public String MypageUpdate() 
 	{
 		return "member/mypage_update";
 	}
-	@RequestMapping(value = "/mypage.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
 	public String Mypage() 
 	{
 		return "member/mypage";
 	}
-	@RequestMapping(value = "/order_detail.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/order_detail", method = RequestMethod.GET)
 	public String OrderDetail() 
 	{
 		return "member/order_detail";
@@ -121,52 +121,52 @@ public class HomeController
 	// member ----------------------------------------------------------
 
 	// shop ----------------------------------------------------------
-	@RequestMapping(value = "/bed.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/bed", method = RequestMethod.GET)
 	public String Bed() 
 	{
 		return "shop/bed";
 	}
-	@RequestMapping(value = "/chair_baige.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/chair_baige", method = RequestMethod.GET)
 	public String ChairBaige() 
 	{
 		return "shop/chair_baige";
 	}
-	@RequestMapping(value = "/chair_green.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/chair_green", method = RequestMethod.GET)
 	public String ChairGreen() 
 	{
 		return "shop/chair_baige";
 	}
-	@RequestMapping(value = "/chair_kruzo.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/chair_kruzo", method = RequestMethod.GET)
 	public String ChairKruzo() 
 	{
 		return "shop/chair_baige";
 	}
-	@RequestMapping(value = "/closet.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/closet", method = RequestMethod.GET)
 	public String Closet() 
 	{
 		return "shop/closet";
 	}
-	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String Main() 
 	{
 		return "shop/main";
 	}
-	@RequestMapping(value = "/mattress.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/mattress", method = RequestMethod.GET)
 	public String Mattress() 
 	{
 		return "shop/mattress";
 	}
-	@RequestMapping(value = "/search.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
 	public String Search() 
 	{
 		return "shop/search";
 	}
-	@RequestMapping(value = "/sofa.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/sofa", method = RequestMethod.GET)
 	public String Sofa() 
 	{
 		return "shop/sofa";
 	}
-	@RequestMapping(value = "/table.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/table", method = RequestMethod.GET)
 	public String Table() 
 	{
 		return "shop/table";
